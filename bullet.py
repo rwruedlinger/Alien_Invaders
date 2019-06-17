c6a8413056ff71b6284ed9dcbf28a6af44b1f5b6
Robert Ruedlinger <wes.ruedlinger91@gmail.com>
2019-06-17 00:01:07 -0500

    commit

--- bullet.py
+++ bullet.py
@@ -1,30 +1,31 @@
 import pygame
 from pygame.sprite import Sprite
-
+ 
 class Bullet(Sprite):
-    """A class to manage bullets fired from the ship."""
+    """A class to manage bullets fired from the ship"""
 
-    def __init__(self, ai_settings, screen, ship):
-        """Create a bullet object at the ships current position."""
+    def __init__(self, ai_game):
+        """Create a bullet object at the ship's current position."""
         super().__init__()
-        self.screen = screen
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
         # Create a bullet rect at (0, 0) and then set correct position.
-        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width,
-            ai_settings.bullet_height)
-        self.rect.centerx = ship.rect.centerx
-        self.rect.top = ship.rect.top
-        # Store bullet's position as a decimal value
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
+            self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+        
+        # Store the bullet's position as a decimal value.
         self.y = float(self.rect.y)
-        self.color = ai_settings.bullet_color
-        self.speed_factor = ai_settings.bullet_speed_factor
 
     def update(self):
         """Move the bullet up the screen."""
         # Update the decimal position of the bullet.
-        self.y -= self.speed_factor
-        # Update the rect position
+        self.y -= self.settings.bullet_speed
+        # Update the rect position.
         self.rect.y = self.y
 
     def draw_bullet(self):
-        """Draw the bullet on the screen"""
+        """Draw the bullet to the screen."""
         pygame.draw.rect(self.screen, self.color, self.rect)
